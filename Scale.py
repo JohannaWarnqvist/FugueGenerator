4f5ac55208556daab512f305200736d105ae84c9
JohannaWarnqvist <johwar@student.chalmers.se>
2020-04-27 16:53:10 +0200

    Added a function to Scale class
    Added a function in the Scale class which returns a list of all notes between two given numbers that are in the scale. Useful for when wanting to randomly choose a tone from the scale.

--- Scale.py
+++ Scale.py
@@ -4,6 +4,7 @@ class Scale:
     def __init__(self, key):
         "Initializes a scale"
 
+        self.key = key
         self.scale_tones = [key, key+2, key + 4, key + 5, key + 7, key + 9, key + 11]
         for tone in self.scale_tones:
             tone = tone % 12
@@ -18,3 +19,19 @@ class Scale:
             if pitch % 12 == tone:
                 return True
         return False    
+        
+    def get_part_of_scale(self, lowest_pitch, highest_pitch):
+        pitch_list = [pitch for pitch in range(lowest_pitch, highest_pitch+1)]
+        
+        i = 0
+        while i < len(pitch_list):
+            pitch_in_scale = self.is_in_scale(pitch_list[i])
+            if not pitch_in_scale:
+                pitch_list.pop(i)
+            else:
+                i += 1
+        
+        return pitch_list
+            
+                
+    
