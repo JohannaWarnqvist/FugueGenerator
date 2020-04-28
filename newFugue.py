857c9434c9bfa50750ceec3fa6cd0b35c41e7343
Emilie Klefbom <Emilie.karon.klefbom@gmail.com>
2020-04-28 21:31:17 +0200

    update to try to allow transpose

--- newFugue.py
+++ newFugue.py
@@ -1,37 +1,58 @@
 #--------------------------------------------------------------
-#This document works as the Main file for fugue generation
-#Contains the global variables that defines the fugue, voices, key and subject
+# This document works as the Main file for fugue generation
+# Contains the global variables that defines the fugue, voices, key and subject
 # The function generate fugue generates a finished fugue composition
+# The subject can be any lenght!
 #--------------------------------------------------------------
 
 from mingus.containers import Composition
 from mingus.containers import Bar
 from mingus.containers import Track
+import mingus.core.intervals as intervals
 
 import mingus.extra.lilypond as LilyPond
 from Mindus_LilyPond_helper import to_LilyPond_file
 import track_functions as Track_Functions
 from mingus.midi import midi_file_out
+import copy
 
 #Important variables!
 fugue = Composition()
 first_voice = Track()
-second_voice = Track()
-subject = Track()
+second_voice = Track() 
+
+#Very useful helper function
+def add_tracks(track1, track2):
+    for i in range(track2.__len__()):  
+        track1.add_bar(track2[i-1])
 
 #input_key is a char signifying what key we are using
-#input_subject is a Track
-def generate_fugue(input_key,input_subject):
-    subject = input_subject
-    key = input_key
+#input_subject is a Track, subject can be any length
+def generate_fugue(key,subject):
+
+    subject_lenght = subject.__len__()
 
     # Create first bar with subject in first voice and pause in second voice. 
-    pause = Bar(key)
-    pause.place_rest(1)
-    first_voice = subject
-    second_voice.add_bar(pause)
+    pause_1bar = Bar(key)
+    pause_1bar.place_rest(1)
+    first_voice = copy.deepcopy(subject)
+    #Kind of ugly way of writing add same amout of "pause bars" as the number of bars in the subject
+    for i in range(subject_lenght):  
+        second_voice.add_bar(pause_1bar)
+    
 
     # Create second bar with answer in second voice. Countersubject comes later.
+    answer = Track_Functions.transpose_from_halfnote(subject , 7)
+    # same as using
+    # answer = copy.deepcopy(subject)
+    # answer = answer.transpose(" 5")
+
+    add_tracks(second_voice,answer)
+
+
+
+    #second_voice = second_voice + answer
+
     # Create development in minor in bar 5 and 6. 
     # Create stretto in bar 9 and 10.
 
@@ -50,6 +71,7 @@ def generate_fugue(input_key,input_subject):
     midi_file_out.write_Composition("final_fugue.mid", fugue)
 
  
+
 #Test for debugging
 test_track = Track_Functions.init_preset_track(1)
 generate_fugue("C",test_track)
--- track_functions.py
+++ track_functions.py
@@ -4,6 +4,9 @@
 #---------------------------------------------
 
 from mingus.containers import Track
+from mingus.containers import Bar
+import mingus.core.intervals as intervals
+import copy
 
 #---------------------------------
 #FUNCTIONS
@@ -17,11 +20,29 @@ def init_preset_track(num):
         track + "A"
         track + "D"
         track + "F"
+        track + "G"
+        track + "E"
     return track
 
-#Transpose
-def transpose(track):
-    return track
+#Transpose - track.transpose(interval, up = True):
+# type_of_interval is for example " 7" = major seventh, "b4" = minor fourth
+# You can use the numbers 1-7 combined with an optional accidental prefix to get the interval from a certain note.
+# Any number of accidentals can be used, but -again- use it cautiously. No prefix means that you want the major interval,
+# a ‘b’ will return the minor interval, ‘bb’ the diminished, ‘#’ the augmented.
+# up is boolean that determines wheter to transpose it up (True) or down (False)
+
+#alt method using nmb_of_halfnotes (an int) as input
+def transpose_from_halfnote(track,nmb_of_halfnotes,up =True):
+    #determine interval from nmb_of_steps
+    lookup = ["b2"," 2","b3"," 3", " 4", "#4", " 5", "b6", " 6", "b7", " 7"]
+    interval = lookup[nmb_of_halfnotes-1]
+    #copy value of reference to prevent changing value of subject
+    transposed_track = copy.deepcopy(track)
+    #calculate transposed track and return
+    transposed_track = transposed_track.transpose(interval,up)
+
+    return transposed_track
+    
 
 #Invert 
 def invert(track):
