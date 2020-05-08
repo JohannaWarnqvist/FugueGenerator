183044e747f8b7b172e739af1905b623a3fb4a22
Emilie Klefbom <Emilie.karon.klefbom@gmail.com>
2020-05-08 12:33:08 +0200

    Added function index and repeating_note_pitch
    Added a function index to easier seewhat functions are implemented in track_test as well as what they do/ if theyre not done. Also added fuinction for calculation repetition of note pitch.

--- track_tests.py
+++ track_tests.py
@@ -16,6 +16,14 @@ import copy
 import random
 import track_functions as Track_Functions
 
+"""FUNCTION INDEX                                           (to be able to find functions easier)
+repeating_note_lenght(track)                                Calculates a fraction between the nmb of notes and the most occuring note length and returns it
+average_numb_of_chords(track1,track2)                       Returns an average numb of chords/bar created by two tracks playing simultaneously
+average_note_length_cluster(track)                          returns the average size of same length note clusters
+repeating_note_pitch(track, (optional bool exact))          Calculates a fraction between the nmb of notes and the most occuring note pitch and returns it
+repeating_passages(track)                                   IN PROGRESS
+"""
+
 #--------------------------------------------------------------------
 # repeating_note_lenght:
 # Calculates a fraction between the nmb of notes and the most occuring note length and returns it
@@ -95,23 +103,54 @@ def average_note_length_clusters(track):
     #return average cluster length
     return (added_cluster_legths/len(cluster_length))
 
+#--------------------------------------------------------------------
+# repeating_note_pitch:
+# Calculates a fraction between the nmb of notes and the most occuring note pitch and returns it
+# Excat is a bool that determines if you differ C-5, C, and Cb or consider them to be the same
+#--------------------------------------------------------------------    
+def repeating_note_pitch(track, exact = False):
+    note_generetor = copy.deepcopy(track).get_notes()
+    #Dictionary that matches note pitch to nmb of occurences of that note
+    note_pitches = {}
+    #nmb of notes in track
+    nmb_of_notes = 0.0
+
+    for note in note_generetor:
+        if note[-1] is None:
+            continue
+        #If note pitch is not in dictionary add it
+        nc = note[-1]
+        for note_pitch in nc:
+            if exact:
+                if not(note_pitch.name in note_pitches):
+                    note_pitches[note_pitch.name] = 0.0
+                note_pitches[note_pitch.name] += 1.0
+            else:
+                if not(note_pitch.name[0] in note_pitches):
+                    note_pitches[note_pitch.name[0]] = 0.0
+                note_pitches[note_pitch.name[0]] += 1.0
+        
+        nmb_of_notes += 1.0
     
+    #Calculate the biggest fraction between occurences and nmb of notes in track
+    biggest_fraction = 0.0
+    for pitches,occurences in note_pitches.items():
+        temp = occurences/nmb_of_notes
+        if temp > biggest_fraction:
+            biggest_fraction = copy.deepcopy(temp)
 
-#Maybe we want functions that generate both bools and a fraction to use for fitness functions 
-#TODO
-#Are notes is scale?
-#Are notes the same?
-#Are note tempos same?
-# Measure interval between track?
-# Measure intervals on beast?
-#Measure repetition
-#Does notes follow progression?
-#Implied harmonic motion 
-#Measure "walking" on sacle
-#Number of chords
-#Circle of fifths?
+    return biggest_fraction
 
-#.....
+#--------------------------------------------------------------------
+# repeating_passages:
+# Calculates average length of repeating passage, 
+# Calculate numb of repetitions of passages larger than 3
+#--------------------------------------------------------------------   
+def repeating_passages(track):
+    note_generetor = copy.deepcopy(track).get_notes()
 
-#Using existing fugues for the comparisons to determine the weights for the different fitness functions
-
+""" Can be used to test functions
+test_track = Track_Functions.init_random_track("C",True)
+print(test_track)
+print("function"(test_track))
+"""
