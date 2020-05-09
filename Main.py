4665e3d23e1895c998c1646dcd8de0434c59abef
JohannaWarnqvist <johwar@student.chalmers.se>
2020-05-09 10:51:26 +0200

    Deleted unused files
    Deleted the files we used before switching to mingus.

--- Main.py
+++
@@ -1,9 +0,0 @@
-from Fugue import Fugue
-
-# Main program
-
-key = 0
-#fugue = Fugue(key)                 # Use random subject
-fugue = Fugue(key, False, 2)        # Use test subject
-    
-fugue.print_fugue()
