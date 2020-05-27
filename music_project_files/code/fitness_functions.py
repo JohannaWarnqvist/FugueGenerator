251f64f8c5f198454b5c54ea92495a6c15523256
Emilie Klefbom <Emilie.karon.klefbom@gmail.com>
2020-05-27 16:57:48 +0200

    folder with final code and music files

---
+++ music_project_files/code/fitness_functions.py
@@ -0,0 +1,230 @@
+# ==============================================
+# Document for all different fitness functions.
+# ==============================================
+
+#import random as rnd
+import numpy as np
+import copy
+#import math
+#from mingus.core import *
+from mingus.containers import *
+import track_tests as measure
+import track_functions as Track_Functions
+#import fitness_functions as Fitness_Functions 
+
+# start of how to give points normalized over number of bars:
+#points = {16: 1/16, 8: 0.5, 16/3: 1/8, 4: 2, 8/3: 0.75, 2: 1, 4/3: 1, 1: 1}
+points = {16:   1/32, 
+          8:    1/4, 
+          16/3: 2/32, 
+          4:    1, 
+          8/3:  1/4, 
+          2:    1, 
+          4/3:  2/4, 
+          1:    1/2}
+        
+accepted_durations = [16, 8, 16/3, 4, 8/3, 2, 4/3, 1]
+
+#VARIABLES TO CHANGE: These are the "perfect" values
+#frac = fraction/percentage of ...., nmb = number of...., rep = repetitions
+frac_repeating_note_length = 0.3   
+nmb_chords_between_tracks = 4.0
+nmb_note_length_clusters = 4.0
+nmb_of_passage_rep = 1.0
+len_of_passage_rep = 3.0
+frac_same_pattern = 0.85
+
+#Helper funtion, creates and adds punishments for differing from "perfect values"
+def near_calc(population_value, perfect_value, bias):
+    return abs(population_value - perfect_value) * bias *(-1.0)
+         
+#The bigger the fraction the bigger the reward/punishment is
+def more_calc(population_fraction, bias):
+    return population_fraction * bias
+
+def calculate_fitness_C(population, nr_bars):
+        """Calculate the fitness for each chromosome and return an array
+        with the fitness values
+        """
+        population_size = len(population)
+        fitness_values = np.zeros(population_size)
+        for iPop in range(population_size):
+            melody = population[iPop]
+            fitness = 0
+            notes = melody.get_notes()
+            
+            for note in notes:
+                if note[-1] is None:
+                        fitness += (1/note[1])*1/nr_bars
+                else:
+                    distance = Note('C').measure(note[-1][0])
+                    fitness += (note[1] * 10*abs(distance))/nr_bars
+
+            if fitness == 0:
+                fitness_values[iPop] = 2
+            else:
+                fitness_values[iPop] = 1/fitness
+        
+        return fitness_values
+
+
+def calculate_fitness_pauses(population):
+    population_size = len(population)
+    fitness_values = np.zeros(population_size)
+    for iPop in range(population_size):
+        melody = population[iPop]
+        fitness = 0
+        notes = melody.get_notes()
+        
+        for note in notes:
+            if note[-1] is None:
+                    fitness += 1/note[1]
+                    
+        fitness_values[iPop] = fitness
+    return fitness_values
+
+    
+# TODO: Create a fitness function for modulating/binding together two bars 
+def calculate_fitness_modulate(population, from_bar, to_bar, from_key, to_key, is_complex = True):
+    "Return a melody that modulates from from_bar to to_bar"
+    
+    if is_complex:
+        # Create a list of melodies including the bar before, the generated melody and the bar after.
+        melodies = []
+        track_from_bar = Track().add_bar(from_bar)
+        track_to_bar = Track().add_bar(to_bar)
+        for melody in population:
+            track = copy.deepcopy(track_from_bar)
+            Track_Functions.add_tracks(track, melody)
+            Track_Functions.add_tracks(track, track_to_bar)
+            melodies.append(track)
+
+        population_size = len(population) 
+        fitness_values = np.zeros(population_size) 
+        
+        default_bias = 10.0
+        #For every melody in population calculate fitness THIS IS THE BIG CALCULATION PART
+        for iPop in range(population_size):
+            melody = melodies[iPop]
+            notes = melody.get_notes()
+            fitness = 1.0
+            #---------------------------------------------------------------------------------------------------------------- 
+            #Unique to modulate 
+
+            #----------------------------------------------------------------------------------------------------------------    
+            #Measure closeness to ideal value
+            
+            #Within the melody
+            #Function that measures:                                                   Ideal value:                    Bias:
+            fitness += near_calc(measure.repeating_note_length(melody),                frac_repeating_note_length,     default_bias)
+            fitness += near_calc(measure.average_note_length_clusters(melody),         nmb_note_length_clusters,       default_bias)
+            (x,y,frac_repeating_passage) = measure.repeating_passages(melody)
+            fitness += near_calc(x,                                                    nmb_of_passage_rep,             default_bias)
+            #when multiplying the default bias we change the rewlative weight of that function
+            fitness += near_calc(y,                                                    len_of_passage_rep,             default_bias *5) 
+            
+            #-----------------------------------------------------------------------------------------------------------------
+            #Function that calculates fraction where 1 is best                          Bias:
+            fitness += more_calc(frac_repeating_passage,                                default_bias) #Calculated previously
+            (on_beat,on_half_beat) = measure.count_notes_on_beat(melody)
+            fitness += more_calc(on_beat,                                               default_bias)
+            fitness += more_calc(on_half_beat,                                          default_bias)
+
+            # Make difference on E and Fb and similar:
+            fitness -= more_calc(measure.repeating_note_pitch(melody,True),           default_bias)
+            fitness -= more_calc(measure.count_tritone_or_seventh_in_two_skips(melody),default_bias)
+                
+            #forgotten functions
+            fitness += more_calc(measure.check_melody_intervals(melody),                default_bias)
+            fitness += more_calc(measure.check_motion_of_melody(melody),                default_bias)
+            # check_durations:
+            durations = measure.check_note_durations(melody)
+            for iDur in accepted_durations:
+                fitness += more_calc(durations[iDur],     points[iDur] *                default_bias)
+
+            #Add resulting fitness value to list
+            fitness_values[iPop] = fitness
+            
+            
+        return fitness_values 
+    else:
+
+        return calculate_fitness_C(population, 2)   
+    
+    
+def calculate_fitness_harmony(population, input_melody, key, counter = False):
+
+    if len(input_melody) == 0:
+        print('Wrong input in fitness function')
+        #breakpoint()
+
+    population_size = len(population) 
+    fitness_values = np.zeros(population_size) 
+
+    default_bias = 10.0
+        
+    #For every melody in population calculate fitness THIS IS THE BIG CALCULATION PART
+    for iPop in range(population_size):
+        melody = population[iPop]
+        notes = melody.get_notes()
+        fitness = 1.0
+        #----------------------------------------------------------------------------------------------------------------    
+        #Measure closeness to ideal value
+        
+        #Within the melody
+        #Function that measures:                                                   Ideal value:                    Bias:
+        fitness += near_calc(measure.repeating_note_length(melody),                frac_repeating_note_length,     default_bias)
+        fitness += near_calc(measure.average_note_length_clusters(melody),         nmb_note_length_clusters,       default_bias)
+        (x,y,frac_repeating_passage) = measure.repeating_passages(melody)
+        fitness += near_calc(x,                                                    nmb_of_passage_rep,             default_bias)
+        fitness += near_calc(y,                                                    len_of_passage_rep,             default_bias)
+        
+        # Between the melodies
+        fitness += near_calc(measure.check_same_pattern(input_melody, melody),     frac_same_pattern,              default_bias)
+        
+        #-----------------------------------------------------------------------------------------------------------------
+        #Function that calculates fraction where 1 is best                          Bias:
+        fitness += more_calc(frac_repeating_passage,                                default_bias) #Calculated previously
+        (on_beat,on_half_beat) = measure.count_notes_on_beat(melody)
+        fitness += more_calc(on_beat,                                               default_bias)
+        fitness += more_calc(on_half_beat,                                          default_bias)
+
+        # This one makes difference between Fb and E:
+        fitness += more_calc(measure.count_notes_in_scale(melody, key),             default_bias)
+        fitness += more_calc(measure.check_same_pattern(input_melody, melody),      default_bias)
+
+        # Check intervals, if consonant or too large
+        consonant, too_long = measure.check_if_intervals_are_consonant_or_too_big(input_melody, melody)
+        fitness += more_calc(consonant,                                             default_bias)
+        fitness -= more_calc(too_long,                                              default_bias)
+
+        # Make difference on E and Fb and similar:
+        fitness -= more_calc(measure.repeating_note_pitch(melody,True),           default_bias)
+        fitness -= more_calc(measure.count_tritone_or_seventh_in_two_skips(melody),default_bias)
+        
+         #forgotten functions
+        fitness += more_calc(measure.check_melody_intervals(melody),                default_bias)
+        fitness += more_calc(measure.check_motion_of_melody(melody),                default_bias)
+               
+        # Contrapunctal motion
+        contrapunctal_motion_values = measure.contrapunctal_motion(input_melody, melody)
+        fitness += more_calc(contrapunctal_motion_values['Contrary'],                default_bias)
+        
+        # If included, should add less than Contrary. Contrary is good, oblique and similar is okay.
+        fitness += more_calc(contrapunctal_motion_values['Oblique'],                default_bias)
+        fitness += more_calc(contrapunctal_motion_values['Similar'],                default_bias)
+        
+        # Having parallel motion or rest in both tracks is considered bad.
+        fitness -= more_calc(contrapunctal_motion_values['Parallel'],               default_bias)
+        fitness -= more_calc(contrapunctal_motion_values['Rest'],                   default_bias)
+               
+        # check_durations:
+        durations = measure.check_note_durations(melody)
+        for iDur in accepted_durations:
+            fitness += more_calc(durations[iDur],     points[iDur]                  * default_bias)
+
+        #Add resulting fitness value to list
+        fitness_values[iPop] = fitness      
+    return fitness_values
+
+    
---
+++ music_project_files/code/fugueGenerator.py
@@ -0,0 +1,343 @@
+#--------------------------------------------------------------
+# This document works as the Main file for fugue generation
+# Contains the global variables that defines the fugue, voices, key and subject
+# The function generate fugue generates a finished fugue composition
+# The subject can be any length!
+#--------------------------------------------------------------
+
+from mingus.containers import Composition
+from mingus.containers import Bar
+from mingus.containers import Track
+import mingus.core.intervals as intervals
+
+import mingus.extra.lilypond as LilyPond
+from Mingus_LilyPond_helper import to_LilyPond_file
+import track_functions as Track_Functions
+from mingus.midi import midi_file_out
+import copy
+from EvolutionaryGenerator import EvolutionaryGenerator
+import random as rnd
+
+#Important variables!
+fugue = Composition()
+first_voice = Track()
+second_voice = Track()     
+            
+#input_key is a char signifying what key we are using
+#input_subject is a Track, subject can be any length
+def generate_fugue(key,subject):
+
+    #If subject doesn't fill full bars fill out rest of last bar of subject with rest
+    #if last bar is not full
+    if not (subject[-1].is_full()): 
+        #place a rest at the end of the last bar with the length of 1/(remaining fraction of bar)
+        subject[-1].place_rest(int(1.0/subject[-1].space_left()))
+
+    # Create first bar with subject in first voice and rest in second voice. 
+    rest_1bar = Bar(key)
+    rest_1bar.place_rest(1)
+    first_voice = copy.deepcopy(subject)
+
+    #Add same amount of "rest bars" as the number of bars in the subject
+    for i in range(len(subject)):  
+        second_voice.add_bar(copy.deepcopy(rest_1bar))
+    
+
+    # Create second bar with answer in second voice.
+    answer = Track_Functions.create_answer(subject, key)
+    
+    #second_voice = second_voice + answer
+    Track_Functions.add_tracks(second_voice,answer)
+    
+    # Generate countersubject
+    eg_counter = EvolutionaryGenerator(key, nr_bars = 1, fitness_function = 'counter', input_melody = subject, nr_generations = counter_nr_generations)
+    eg_counter.run_evolution()
+    counter_subject = copy.deepcopy(eg_counter.best_individual)
+    
+    Track_Functions.add_tracks(first_voice, counter_subject)
+    
+
+    # Save bar 2 for later modulation
+    bar_2 = first_voice[-1]    
+    
+    # Generate development in minor in bar 5 and 6. 
+    # Transposed -3 to minor + (stämma i för second voice tills vidare tom)
+    minor_first_voice = Track_Functions.transpose_to_relative_minor(first_voice, key, False)
+    minor_second_voice = Track_Functions.transpose_to_relative_minor(second_voice, key, False)
+        
+    bar_5 = minor_first_voice[0]
+    
+    # Generate harmony in second voice in bar 5
+    eg_harmony_minor = EvolutionaryGenerator(key, nr_bars = 1, fitness_function = 'harmony', 
+            input_melody = Track().add_bar(copy.deepcopy(minor_first_voice[0])), nr_generations = harmony_nr_generations)
+
+    eg_harmony_minor.run_evolution()
+    
+    minor_second_voice[0] = eg_harmony_minor.best_individual[0]
+
+    # Generate bar 3 and 4 as a modulation between bar 2 and 5
+    minor_key = intervals.from_shorthand(key, 'b3', False)
+    minor_key += 'm'
+
+    eg_modulate_to_minor = EvolutionaryGenerator(key, nr_bars = 2, fitness_function = 'modulate', 
+            from_bar = bar_2, to_bar = bar_5, from_key = key, to_key = minor_key, nr_generations = modulate_nr_generations)
+
+    eg_modulate_to_minor.run_evolution()
+    modulate_first_voice = copy.deepcopy(eg_modulate_to_minor.best_individual)
+
+    # Generate second voice as harmony to the first voice in bar 3 and 4
+    
+    eg_second_voice_modulate = EvolutionaryGenerator(key, nr_bars = 2, fitness_function = 'harmony', 
+            input_melody = modulate_first_voice, from_key = 'C', to_key = 'Am', nr_generations = harmony_nr_generations)
+    
+    eg_second_voice_modulate.run_evolution()    
+    modulate_second_voice = copy.deepcopy(eg_second_voice_modulate.best_individual)
+
+
+    # Add bar 3-6 to the voice tracks
+    Track_Functions.add_tracks(first_voice, modulate_first_voice)
+    Track_Functions.add_tracks(second_voice, modulate_second_voice)
+    
+    Track_Functions.add_tracks(first_voice, minor_first_voice)
+    Track_Functions.add_tracks(second_voice, minor_second_voice)
+
+    bar_6 = first_voice[-1]
+
+    # Create canon in bar 9 and 10.
+    # subject i first voice
+    # second voice is subject but shifted (half a bar for now) 
+
+    canon_first_voice = Track()
+    canon_first_voice.add_bar(copy.deepcopy(subject[0]))
+    
+    bar_9 = canon_first_voice[0]
+
+    canon_second_voice = Track_Functions.shift(subject, 2)
+
+    # Create modulation from minor to major in 7 and 8
+    
+    eg_modulate_to_major = EvolutionaryGenerator(key, nr_bars = 2, fitness_function = 'modulate', 
+            from_bar = bar_6, to_bar = bar_9, from_key = 'Am', to_key = 'C', nr_generations = modulate_nr_generations)
+
+    eg_modulate_to_major.run_evolution()
+    modulate_back_first_voice = copy.deepcopy(eg_modulate_to_major.best_individual)
+
+    # Generate second voice as harmony to the first voice in bar 7 and 8
+    
+    eg_second_voice_modulate_back = EvolutionaryGenerator(key, nr_bars = 2, fitness_function = 'harmony', 
+            input_melody = modulate_first_voice, from_key = 'Am', to_key = 'C', nr_generations = harmony_nr_generations)
+    
+    eg_second_voice_modulate_back.run_evolution()    
+    modulate_back_second_voice = copy.deepcopy(eg_second_voice_modulate.best_individual)
+    
+    
+    # Add bar 7-10 to the voice tracks
+    Track_Functions.add_tracks(first_voice, modulate_back_first_voice)
+    Track_Functions.add_tracks(second_voice, modulate_back_second_voice)
+    
+    Track_Functions.add_tracks(first_voice, canon_first_voice)
+    Track_Functions.add_tracks(second_voice, canon_second_voice)
+
+    Track_Functions.ending(first_voice, second_voice, subject, key)
+    
+    #Add voices together to create a final composition
+    fugue.add_track(first_voice)
+    fugue.add_track(second_voice)
+
+    #Generate lilypond file for fugue named final_fugue
+    finished_fugue = LilyPond.from_Composition(fugue)
+    to_LilyPond_file(finished_fugue,"final_fugue")
+
+    #Generate MIDI output for fugue named final_fugue
+    midi_file_out.write_Composition("final_fugue.mid", fugue)
+
+ 
+# nr_parts tells how many parts (inverse, reverse, minor, other start note) is wanted between first subject/answer and the last stretto.
+def generate_random_fugue(key, subject, nr_parts = 1, order_of_parts = None):
+    #If subject doesn't fill full bars fill out rest of last bar of subject with rest
+    #if last bar is not full
+    if not (subject[-1].is_full()): 
+        #place a rest at the end of the last bar with the length of 1/(remaining fraction of bar)
+        subject[-1].place_rest(int(1.0/subject[-1].space_left()))
+
+    # Create first bar with subject in first voice and rest in second voice. 
+    rest_1bar = Bar(key)
+    rest_1bar.place_rest(1)
+    first_voice = copy.deepcopy(subject)
+
+    #Add same amount of "rest bars" as the number of bars in the subject
+    for i in range(len(subject)):  
+        second_voice.add_bar(copy.deepcopy(rest_1bar))
+    
+
+    # Create second bar with answer in second voice.
+    answer = Track_Functions.create_answer(subject, key)
+    
+    Track_Functions.add_tracks(second_voice,answer)
+    
+    # Generate countersubject
+    eg_counter = EvolutionaryGenerator(key, nr_bars = 1, fitness_function = 'counter', input_melody = subject, nr_generations = counter_nr_generations)
+    eg_counter.run_evolution()
+    counter_subject = copy.deepcopy(eg_counter.best_individual)
+    
+    Track_Functions.add_tracks(first_voice, counter_subject)
+    
+
+    # Save subject, answer and countersubject
+    first_voice_first_part = copy.deepcopy(first_voice)
+    second_voice_first_part = copy.deepcopy(second_voice)
+    
+    # Save bar 2 for later modulation
+    bar_prev = first_voice[-1]
+
+    variants = ['Minor', 'Reverse', 'Inverse']
+    iParts = 0
+    
+    if order_of_parts is None:
+        order_of_parts = []
+        for i in range(nr_parts):
+            rVariant = rnd.choice(variants)
+            order_of_parts.append(rVariant)
+    
+    
+    while iParts < nr_parts:
+            current_variant = order_of_parts[iParts]
+            
+            if current_variant == 'Minor':
+                # Generate development in minor
+                # Transposed -3 to minor (stämma i second voice tills vidare tom)
+                new_first_voice = Track_Functions.transpose_to_relative_minor(first_voice, key, False)
+                new_second_voice = Track_Functions.transpose_to_relative_minor(second_voice, key, False)
+                    
+                bar_after = new_first_voice[0]
+            
+                # Generate harmony in second voice first bar
+                eg_harmony = EvolutionaryGenerator(key, nr_bars = 1, fitness_function = 'harmony', 
+                        input_melody = Track().add_bar(copy.deepcopy(new_first_voice[0])), nr_generations = harmony_nr_generations)
+
+                eg_harmony.run_evolution()
+                
+                new_second_voice[0] = eg_harmony.best_individual[0]
+
+            elif current_variant == 'Reverse':
+                # Genereate reverse development
+                
+                new_first_voice = Track_Functions.reverse(first_voice_first_part, key)
+                new_second_voice = Track_Functions.reverse(second_voice_first_part, key)
+                
+                bar_after = new_first_voice[0]
+
+                # Generate harmony in second voice first bar
+                eg_harmony = EvolutionaryGenerator(key, nr_bars = 1, fitness_function = 'harmony', 
+                        input_melody = Track().add_bar(copy.deepcopy(new_first_voice[1])), nr_generations = harmony_nr_generations)
+
+                eg_harmony.run_evolution()
+                #breakpoint()
+                new_second_voice[1] = eg_harmony.best_individual[0]
+
+            elif current_variant == 'Inverse':
+                # Genereate inverse development
+                
+                new_first_voice = Track_Functions.inverse(first_voice_first_part)
+                new_second_voice = Track_Functions.inverse(second_voice_first_part)
+                
+                bar_after = new_first_voice[0]
+
+                # Generate harmony in second voice first bar
+                eg_harmony = EvolutionaryGenerator(key, nr_bars = 1, fitness_function = 'harmony', 
+                        input_melody = Track().add_bar(copy.deepcopy(new_first_voice[0])), nr_generations = harmony_nr_generations)
+
+                eg_harmony.run_evolution()
+                #breakpoint()
+                new_second_voice[0] = eg_harmony.best_individual[0]
+                
+                
+
+            # Generate the two bars linking this new part to the previous parts
+
+            eg_modulate = EvolutionaryGenerator(key, nr_bars = 2, fitness_function = 'modulate', 
+                    from_bar = bar_prev, to_bar = bar_after, nr_generations = modulate_nr_generations)
+
+            eg_modulate.run_evolution()
+            modulate_first_voice = copy.deepcopy(eg_modulate.best_individual)
+
+            # Generate second voice as harmony to this linking part
+            
+            eg_second_voice_modulate = EvolutionaryGenerator(key, nr_bars = 2, fitness_function = 'harmony', 
+                    input_melody = modulate_first_voice, nr_generations = harmony_nr_generations)
+            
+            eg_second_voice_modulate.run_evolution()    
+            modulate_second_voice = copy.deepcopy(eg_second_voice_modulate.best_individual)
+
+
+            # Add new bars to the voice tracks
+            Track_Functions.add_tracks(first_voice, modulate_first_voice)
+            Track_Functions.add_tracks(second_voice, modulate_second_voice)
+            
+            Track_Functions.add_tracks(first_voice, new_first_voice)
+            Track_Functions.add_tracks(second_voice, new_second_voice)
+
+            bar_prev = first_voice[-1]
+            
+            iParts += 1
+            
+
+    # Create canon in bar 9 and 10.
+    # subject i first voice
+    # second voice is subject but shifted (half a bar for now) 
+
+    canon_first_voice = Track()
+    canon_first_voice.add_bar(copy.deepcopy(subject[0]))
+    
+    bar_after = canon_first_voice[0]
+
+    canon_second_voice = Track_Functions.shift(subject, 2)
+
+    # Create modulation from minor to major in 7 and 8
+    
+    eg_modulate_to_major = EvolutionaryGenerator(key, nr_bars = 2, fitness_function = 'modulate', 
+            from_bar = bar_prev, to_bar = bar_after, nr_generations = modulate_nr_generations)
+
+    eg_modulate_to_major.run_evolution()
+    modulate_back_first_voice = copy.deepcopy(eg_modulate_to_major.best_individual)
+
+    # Generate second voice as harmony to the first voice in bar 7 and 8
+    
+    eg_second_voice_modulate_back = EvolutionaryGenerator(key, nr_bars = 2, fitness_function = 'harmony', 
+            input_melody = modulate_first_voice, nr_generations = harmony_nr_generations)
+    
+    eg_second_voice_modulate_back.run_evolution()    
+    modulate_back_second_voice = copy.deepcopy(eg_second_voice_modulate.best_individual)
+    
+    
+    # Add bar 7-10 to the voice tracks
+    Track_Functions.add_tracks(first_voice, modulate_back_first_voice)
+    Track_Functions.add_tracks(second_voice, modulate_back_second_voice)
+    
+    Track_Functions.add_tracks(first_voice, canon_first_voice)
+    Track_Functions.add_tracks(second_voice, canon_second_voice)
+
+    Track_Functions.ending(first_voice, second_voice, subject, key)
+    
+    #Add voices together to create a final composition
+    fugue.add_track(first_voice)
+    fugue.add_track(second_voice)
+
+    #Generate lilypond file for fugue named final_fugue (removed for submission)
+    #finished_fugue = LilyPond.from_Composition(fugue)
+    #to_LilyPond_file(finished_fugue,"final_fugue")
+
+    #Generate MIDI output for fugue named final_fugue
+    midi_file_out.write_Composition("final_fugue.mid", fugue) 
+    return
+
+ 
+# Set number of generations to use for different types of generators
+harmony_nr_generations = 200
+modulate_nr_generations = 100
+counter_nr_generations = 200
+
+# Test for debugging
+test_track = Track_Functions.init_preset_track("blinka")
+generate_random_fugue("C", test_track, 3, ['Minor', 'Inverse', 'Reverse'])
+
